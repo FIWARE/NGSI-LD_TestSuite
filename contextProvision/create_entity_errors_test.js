c50f9cbc1e19d7d141c982dfbb1f1688848eadda
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-08-17 13:11:03 +0200

    Linting

--- contextProvision/create_entity_errors_test.js
+++ contextProvision/create_entity_errors_test.js
@@ -3,29 +3,29 @@
 var testedResource = require('../common.js').testedResource;
 var http = require('../http.js');
 
-var entitiesResource = testedResource + "/" + "entities" + "/";
+var entitiesResource = testedResource + '/' + 'entities' + '/';
 
 describe('Create Entity. Errors. JSON', () => {
-    it("should reject an entity which id is not a URI", async function () {
+    it('should reject an entity which id is not a URI', async function() {
         let entity = {
-          "id": "abcdef",
-          "type": "T"
+          'id': 'abcdef',
+          'type': 'T'
         };
-        
+
         let response = await http.post(entitiesResource, entity);
         expect(response.response).toHaveProperty('statusCode', 400);
     });
-    
-    it("should reject an empty which node type is not recognized", async function () {
+
+    it('should reject an empty which node type is not recognized', async function() {
         let entity = {
-          "id": "urn:ngsi-ld:T4:9000",
-          "type": "T",
-          "P1": {
-            "type": "abcdef",
-            "value": 34
+          'id': 'urn:ngsi-ld:T4:9000',
+          'type': 'T',
+          'P1': {
+            'type': 'abcdef',
+            'value': 34
           }
         };
-        
+
         let response = await http.post(entitiesResource, entity);
         expect(response.response).toHaveProperty('statusCode', 400);
     });
--- contextProvision/create_entity_with_ldcontext_test.js
+++ contextProvision/create_entity_with_ldcontext_test.js
@@ -3,71 +3,71 @@
 var testedResource = require('../common.js').testedResource;
 var http = require('../http.js');
 
-var entitiesResource = testedResource + "/" + "entities" + "/";
+var entitiesResource = testedResource + '/' + 'entities' + '/';
 
 const JSON_LD_HEADERS = {
     'Content-Type': 'application/ld+json'
 };
 
-describe('Create Entity. JSON-LD @context', () => {   
-    it("should create an entity with JSON-LD @context as single URI", async function () {
+describe('Create Entity. JSON-LD @context', () => {
+    it('should create an entity with JSON-LD @context as single URI', async function() {
         let entity = {
-          "id": "urn:ngsi-ld:T:I12345" + ":" + new Date().getTime(),
-          "type": "T",
-          "P1": {
-            "type": "TemporalProperty",
-            "value": "2018-12-04T12:00:00"
+          'id': 'urn:ngsi-ld:T:I12345' + ':' + new Date().getTime(),
+          'type': 'T',
+          'P1': {
+            'type': 'TemporalProperty',
+            'value': '2018-12-04T12:00:00'
           },
-          "P2": {
-            "type": "GeoProperty",
-            "value": {
-                "type": "Point",
-                "coordinates": [-8,40]
+          'P2': {
+            'type': 'GeoProperty',
+            'value': {
+                'type': 'Point',
+                'coordinates': [-8, 40]
             }
           },
-           "P1": {
-            "type": "Property",
-            "value": "Hola"
+           'P1': {
+            'type': 'Property',
+            'value': 'Hola'
           },
-          "R1": {
-            "type": "Relationship",
-            "object": "urn:ngsi-ld:T2:6789"
+          'R1': {
+            'type': 'Relationship',
+            'object': 'urn:ngsi-ld:T2:6789'
           },
-          "@context": "http://fiware.github.io/NGSI-LD_Tests/context/testContext.jsonld"
+          '@context': 'http://fiware.github.io/NGSI-LD_Tests/context/testContext.jsonld'
         };
-       
+
         let response = await http.post(entitiesResource, entity, JSON_LD_HEADERS);
-        expect(response.response).toHaveProperty('statusCode', 201);   
+        expect(response.response).toHaveProperty('statusCode', 201);
     });
-    
-    it("should create an entity with JSON-LD @context as single URI", async function () {
+
+    it('should create an entity with JSON-LD @context as single URI', async function() {
         let entity = {
-          "id": "urn:ngsi-ld:T:I12345" + ":" + new Date().getTime(),
-          "type": "T",
-          "P1": {
-            "type": "TemporalProperty",
-            "value": "2018-12-04T12:00:00"
+          'id': 'urn:ngsi-ld:T:I12345' + ':' + new Date().getTime(),
+          'type': 'T',
+          'P1': {
+            'type': 'TemporalProperty',
+            'value': '2018-12-04T12:00:00'
           },
-          "P2": {
-            "type": "GeoProperty",
-            "value": {
-                "type": "Point",
-                "coordinates": [-8,40]
+          'P2': {
+            'type': 'GeoProperty',
+            'value': {
+                'type': 'Point',
+                'coordinates': [-8, 40]
             }
           },
-           "P1": {
-            "type": "Property",
-            "value": "Hola"
+           'P1': {
+            'type': 'Property',
+            'value': 'Hola'
           },
-          "R1": {
-            "type": "Relationship",
-            "object": "urn:ngsi-ld:T2:6789"
+          'R1': {
+            'type': 'Relationship',
+            'object': 'urn:ngsi-ld:T2:6789'
           },
-          "@context": ["http://fiware.github.io/NGSI-LD_Tests/context/testContext.jsonld"]
+          '@context': ['http://fiware.github.io/NGSI-LD_Tests/context/testContext.jsonld']
         };
-       
+
         let response = await http.post(entitiesResource, entity, JSON_LD_HEADERS);
-        expect(response.response).toHaveProperty('statusCode', 201);   
+        expect(response.response).toHaveProperty('statusCode', 201);
     });
-    
+
 });
