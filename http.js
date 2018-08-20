d135d36f3d44cb80fc28303159cd21c51bea81e9
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-08-20 10:53:27 +0200

    Full Context

--- http.js
+++ http.js
@@ -20,6 +20,25 @@ function post(resource, data, headers) {
     });
 }
 
+function get(resource, headers) {
+    return new Promise(function(resolve, reject) {
+        let options = {
+            method: 'GET',
+            uri: resource,
+            headers: headers,
+            json:true
+        };
+
+        request(options, function(error, response, body) {
+            resolve({
+                response: response,
+                body: body
+            });
+        });
+    });
+}
+
 module.exports = {
-    'post': post
+    'post': post,
+    'get': get
 };
