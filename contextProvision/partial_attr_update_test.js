8f0223608846362e98a7008d23d7faac09d0ec86
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-09-11 12:29:55 +0200

    Towards eslint support

--- contextProvision/partial_attr_update_test.js
+++ contextProvision/partial_attr_update_test.js
@@ -13,7 +13,7 @@ describe('Partial Entity Attribute Update. JSON. Default @context', () => {
         'P1': {
             'type': 'Property',
             'value': 12,
-            'observedAt': '2018-12-04T12:00:00.00',
+            'observedAt': '2018-12-04T12:00:00',
             'P1_R1': {
                 'type': 'Relationship',
                 'object': 'urn:ngsi-ld:T2:6789'
