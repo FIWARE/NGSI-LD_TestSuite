c2fb5d04850219da9d7c9dfeb51bfd09a3a4bdef
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2019-01-08 12:35:20 +0100

    Add subscription with LD @context. Delete Subscription

---
+++ contextSubscription/create_subscription_with_ldcontext_test.js
@@ -0,0 +1,44 @@
+const testedResource = require('../common.js').testedResource;
+const assertSubscriptionCreated = require('../common.js').assertSubscriptionCreated;
+const http = require('../http.js');
+
+const subscriptionsResource = testedResource + '/subscriptions/';
+
+const JSON_LD_HEADERS = {
+  'Content-Type': 'application/ld+json'
+};
+
+describe('Create Subscription. JSON-LD @context', () => {
+  
+  it('should create an entity with JSON-LD @context as single URI', async function() {
+    const subscription = {
+      'id': 'urn:ngsi-ld:Subscription:' + new Date().getTime(),
+      'type': 'Subscription',
+      'entities': [
+        {
+          'type': 'T'
+        }
+      ],
+      'watchedAttributes': ['P1'],
+      'q': 'P1>50',
+      'geoQ': {
+        'georel': 'near;maxDistance==2000',
+        'geometry': 'Point',
+        'coordinates': [-1, 100]
+      },
+      'notification': {
+        'attributes': ['P2'],
+        'format': 'keyValues',
+        'endpoint': {
+          'uri': 'http://my.endpoint.org/notify',
+          'accept': 'application/json'
+        }
+      },
+      '@context': 'https://fiware.github.io/NGSI-LD_Tests/ldContext/testFullContext.jsonld'
+    };
+
+    const response = await http.post(subscriptionsResource, subscription, JSON_LD_HEADERS);
+    assertSubscriptionCreated(response.response, subscription.id);
+  });
+  
+});
