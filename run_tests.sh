bb8d0a4bcbe50b0b989e732a2fbe46c427848093
Jason Fox <jason.fox@fiware.org>
2020-01-02 17:24:35 +0100

    Remove dead code.

--- run_tests.sh
+++ run_tests.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-if [ -f accumulator.pid ]; then
-  # Ensure the Accumulator is not already running
-  process_exist=`pgrep -F accumulator.pid`
-  old_process=`cat accumulator.pid`
-
-  if [ "$process_exist" ==  "$old_process" ]; then
-    kill -9 `cat accumulator.pid`
-    rm accumulator.pid
-  fi
-fi
-
 ACC_ENDPOINT=${ACC_ENDPOINT:-http://localhost:8080}
 TEST_ENDPOINT=${TEST_ENDPOINT:-http://localhost:1026}
 NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://accumulator:8080/acc}
 #NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://host.docker.internal:8080/acc}
 
-
-#echo "Starting accumulator ..."
-#node ./notifications/accumulator.js $ACC_ENDPOINT > accumulator.log & 
-#echo $! > ./accumulator.pid
-#
-#sleep 2
-
 echo "Accumulator started ... at $ACC_ENDPOINT"
 echo "NGSI-LD Broker endpoint ... at $TEST_ENDPOINT"
 echo "The notification endpoint ... at $NOTIFY_ENDPOINT"
-
-#./services start
 
 while [ `curl -s -o /dev/null -w %{http_code} $TEST_ENDPOINT` -eq 000 ]
 do 
