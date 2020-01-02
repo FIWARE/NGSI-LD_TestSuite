6aff578184943edfd0dba64cebf2b70f1b55d67c
Jason Fox <jason.fox@fiware.org>
2020-01-02 17:29:52 +0100

    Tidying start script.

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 ACC_ENDPOINT=${ACC_ENDPOINT:-http://localhost:8080}
-TEST_ENDPOINT=${TEST_ENDPOINT:-http://localhost:1026}
 NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://accumulator:8080/acc}
 #NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://host.docker.internal:8080/acc}
 
 echo "Accumulator started ... at $ACC_ENDPOINT"
-echo "NGSI-LD Broker endpoint ... at $TEST_ENDPOINT"
 echo "The notification endpoint ... at $NOTIFY_ENDPOINT"
+
+while [ `curl -s -o /dev/null -w %{http_code} $ACC_ENDPOINT` -eq 000 ]
+do 
+  echo -e "Accumulator HTTP state: " `curl -s -o /dev/null -w %{http_code} $ACC_ENDPOINT` " (waiting for 200)"
+  sleep 1
+done
+
+
+TEST_ENDPOINT=${TEST_ENDPOINT:-http://localhost:1026}
+echo "NGSI-LD Broker endpoint ... at $TEST_ENDPOINT"
 
 while [ `curl -s -o /dev/null -w %{http_code} $TEST_ENDPOINT` -eq 000 ]
 do 
