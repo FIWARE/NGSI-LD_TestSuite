dbe78124d9077dbc75af3104f12c941fd93c74c2
Jason Fox <jason-fox@users.noreply.github.com>
2020-01-02 18:45:59 +0100

    Add instructive text

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,13 @@ ACC_ENDPOINT=${ACC_ENDPOINT:-http://localhost:8080}
 NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://accumulator:8080/acc}
 #NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://host.docker.internal:8080/acc}
 
-echo "Accumulator started ... at $ACC_ENDPOINT"
-echo "The notification endpoint ... at $NOTIFY_ENDPOINT"
+echo "The Accumulator endpoint listens on $ACC_ENDPOINT"
+echo "Subscriptions will notify the accumulator using $NOTIFY_ENDPOINT"
+
+# To run an accumulator locally run:
+#
+# export WEB_APP_PORT=3000
+# node accumulator/accumulator.js
 
 while [ `curl -s -o /dev/null -w %{http_code} $ACC_ENDPOINT` -eq 000 ]
 do 
