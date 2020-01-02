926edd55a50090d2da20ab3f48b92d761de79876
Jason Fox <jason-fox@users.noreply.github.com>
2020-01-02 17:51:12 +0100

    Feature/dockerize accumulator (#28)
    Dockerize the accumulator

--- run_tests.sh
+++ run_tests.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
 
-if [ -f accumulator.pid ]; then
-  # Ensure the Accumulator is not already running
-  process_exist=`pgrep -F accumulator.pid`
-  old_process=`cat accumulator.pid`
-
-  if [ "$process_exist" ==  "$old_process" ]; then
-    kill -9 `cat accumulator.pid`
-    rm accumulator.pid
-  fi
-fi
-
-ACC_ENDPOINT=${ACC_ENDPOINT:-http://localhost:3000}
-TEST_ENDPOINT=${TEST_ENDPOINT:-http://localhost:1026}
-NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://host.docker.internal:3000/acc}
-
-echo "Starting accumulator ..."
-node ./notifications/accumulator.js $ACC_ENDPOINT > accumulator.log & 
-echo $! > ./accumulator.pid
-
-sleep 2
+ACC_ENDPOINT=${ACC_ENDPOINT:-http://localhost:8080}
+NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://accumulator:8080/acc}
+#NOTIFY_ENDPOINT=${NOTIFY_ENDPOINT:-http://host.docker.internal:8080/acc}
 
 echo "Accumulator started ... at $ACC_ENDPOINT"
-echo "NGSI-LD Broker endpoint ... at $TEST_ENDPOINT"
 echo "The notification endpoint ... at $NOTIFY_ENDPOINT"
 
-#./services start
+while [ `curl -s -o /dev/null -w %{http_code} $ACC_ENDPOINT` -eq 000 ]
+do 
+  echo -e "Accumulator HTTP state: " `curl -s -o /dev/null -w %{http_code} $ACC_ENDPOINT` " (waiting for 200)"
+  sleep 1
+done
+
+
+TEST_ENDPOINT=${TEST_ENDPOINT:-http://localhost:1026}
+echo "NGSI-LD Broker endpoint ... at $TEST_ENDPOINT"
 
 while [ `curl -s -o /dev/null -w %{http_code} $TEST_ENDPOINT` -eq 000 ]
 do 
@@ -34,6 +24,3 @@ do
 done
 
 jest  --runInBand --verbose
-
-# Killing the Accumulator
-curl -X POST $ACC_ENDPOINT/kill 
